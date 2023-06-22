b9d44a65a175c1e40d1645a31ae5a1933e3e755a
Beqa Benidze <beqabenidze9@gmail.com>
2023-06-22 05:06:32 +0400

    github

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         </Helmet>
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio/" element={<Home />} />
           {/* <Route path="/about" element={<About />} /> */}
         </Routes>
       </ThemeProvider>
