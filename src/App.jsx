ff27c1210eae6d720f9fc43152d2435d0b0d7c48
Beqa Benidze <beqabenidze9@gmail.com>
2023-07-02 18:49:51 +0400

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       </Helmet>
 
       <Routes>
-        <Route path="/portfolio/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </ThemeProvider>
   );
